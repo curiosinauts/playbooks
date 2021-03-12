598693e74f6fa32f71e7a5de9b5a5cec9736dfbc
Seven Tella <7onetella@gmail.com>
2021-03-12 12:58:06 -0500

    adding tls to fabio

--- playbooks/roles/fabio/files/fabio.sh
+++ playbooks/roles/fabio/files/fabio.sh
@@ -6,7 +6,8 @@ bind_addr=$(ip address | grep eth1 -A 1 | grep inet | awk '{ print $2 }' | cut -
 
 # start fabio
 /usr/local/bin/fabio \
---proxy.addr "${bind_addr}:80;rt=3s" \
+--proxy.addr "${bind_addr}:443;cs=mystore;rt=3s" \
 --ui.addr "${bind_addr}:9998" \
 --registry.consul.register.addr "${bind_addr}:9998" \
---tracing.SpanHost "${bind_addr}:9998"
+--tracing.SpanHost "${bind_addr}:9998" \
+-cfg /etc/fabio.d/fabio.conf
