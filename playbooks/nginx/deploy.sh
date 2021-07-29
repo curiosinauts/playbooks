a782e6802ee1b3c279b3d21c36d3e51b653666f9
Seven Tella <7onetella@gmail.com>
2021-07-29 13:08:03 -0400

    adding nginx

---
+++ playbooks/nginx/deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+
+# set -x
+
+usage() {
+  echo -e "Usage   : deloy.sh"  
+  echo
+  echo -e 'Example : deloy.sh"'
+  echo
+}
+
+export ANSIBLE_STDOUT_CALLBACK=debug
+export ANSIBLE_ROLES_PATH=./roles:../roles
+
+# if [ "${1}" = "" ]; then
+#     usage
+#     exit 0
+# fi
+
+echo
+read -p "Enter node ip address : " ip_address
+ip_address=${ip_address:-192.168.0.111}
+
+read -p "Enter ansible_user [debian]: " ansible_user
+ansible_user=${ansible_user:-debian}
+
+cat hosts.tpl > hosts
+echo "${ip_address}" >> hosts 
+
+ansible-playbook -i hosts -e "ansible_user=${ansible_user}" nginx.yml    
