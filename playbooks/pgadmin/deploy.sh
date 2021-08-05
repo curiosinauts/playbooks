89604bd029d62af40c4cd5e9c915975ca58d7692
7onetella <7onetella@gmail.com>
2021-08-04 23:04:42 -0400

    adding pgadmin

---
+++ playbooks/pgadmin/deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+
+# set -x
+
+usage() {
+  echo -e "Usage   : deloy.sh"  
+  echo
+  echo -e 'Example : deloy.sh'
+  echo
+}
+
+export ANSIBLE_STDOUT_CALLBACK=debug
+export ANSIBLE_ROLES_PATH=./roles:../roles:~/.ansible/roles
+
+# if [ "${1}" = "" ]; then
+#     usage
+#     exit 0
+# fi
+
+
+echo
+read -p "Enter node ip address         : " ip_address
+echo
+read -p "Enter ansible_user [debian]   : " ansible_user
+
+ip_address=${ip_address:-192.168.0.112}
+ansible_user=${ansible_user:-debian}
+
+cat ../hosts.tpl > hosts
+echo "${ip_address}" >> hosts
+
+ansible-playbook -i hosts -e "ansible_user=${ansible_user}" pgadmin.yml    
