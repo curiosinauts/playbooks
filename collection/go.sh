dd96c3f4bbf1f1a460c66f56ccbdc66e2ebb5f1d
Scott Seo <scott.seo@viacom.com>
2021-08-20 23:55:58 -0400

    updating go install

---
+++ collection/go.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -e
+
+# set -x
+
+usage() {
+  echo -e "Usage   : deloy.sh [<setup_option>]"  
+  echo
+  echo -e "Enter jenkins hostname            : "
+}
+
+export ANSIBLE_STDOUT_CALLBACK=debug
+export ANSIBLE_ROLES_PATH=./roles:../roles:~/.ansible/roles
+
+if [ "${1}" = "-h" ]; then
+    usage
+    exit 0
+fi
+
+echo
+read -p "Enter node ip address             : " ip_address
+echo
+read -p "Enter ansible user     [debian]   : " ansible_user
+echo
+read -p "Enter  user            [debian]   : " user
+echo
+read -p "Enter  go version      [1.16.7]   : " version
+
+ip_address=${ip_address:-192.168.0.108}
+ansible_user=${ansible_user:-debian}
+user=${user:-debigna}
+version=${version:-1.16.7}
+
+cat ./hosts.tpl > hosts
+echo "${ip_address}" >> hosts
+
+ansible-playbook -i hosts -e "ansible_user=${ansible_user} user=${user} version=${version}" go.yml    
+
+rm -f hosts || true
