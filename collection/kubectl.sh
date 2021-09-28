1d2ed5eb6e20cb5d45e17d3fdb5234b0c0489cf8
Scott Seo <scott.seo@viacom.com>
2021-09-27 23:32:03 -0400

    adding kubectl

---
+++ collection/kubectl.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+
+# set -x
+
+usage() {
+  echo -e "Usage   : deloy.sh"  
+}
+
+export ANSIBLE_STDOUT_CALLBACK=debug
+export ANSIBLE_ROLES_PATH=./roles:../roles:~/.ansible/roles
+
+if [ "${1}" = "-h" ]; then
+    usage
+    exit 0
+fi
+
+echo
+read -p "Enter node ip address             : " ip_address
+echo
+read -p "Enter ansible user     [debian]   : " ansible_user
+
+ip_address=${ip_address:-192.168.0.119}
+ansible_user=${ansible_user:-debian}
+
+cat ./hosts.tpl > hosts
+echo "${ip_address}" >> hosts
+
+ansible-playbook -i hosts -e "ansible_user=${ansible_user}" kubectl.yml    
+
+rm -f hosts || true
