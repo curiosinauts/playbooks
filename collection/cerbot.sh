843426f24d6099d8bfe3891ba513b7f9970060e2
little-unit-9283 <little-unit-9283@curiosityworks.org>
2021-10-07 21:28:22 -0400

    updating cerbot

--- collection/cerbot.sh
+++ collection/cerbot.sh
@@ -18,19 +18,19 @@ export ANSIBLE_ROLES_PATH=./roles:../roles
 # fi
 
 echo
-read -p "Enter node ip address       : " ip_address
+read -p "Enter node ip address  [192.168.0.111]       : " ip_address
 ip_address=${ip_address:-192.168.0.111}
 
 echo
-read -p "Enter ansible_user [debian] : " ansible_user
+read -p "Enter ansible_user     [debian]              : " ansible_user
 ansible_user=${ansible_user:-debian}
 
 echo
-read -p "Enter email                 : " email
+read -p "Enter email            [7onetella@gmail.com] : " email
 email=${email:-7onetella@gmail.com}
 
 echo
-read -p "Enter api_key               : " api_key
+read -p "Enter cloudflare api_key                     : " api_key
 
 cat ../hosts.tpl > hosts
 echo "${ip_address}" >> hosts 
