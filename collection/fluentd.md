7516d54cbcf880b711a278da324ab02604772e40
little-unit-9283 <little-unit-9283@curiosityworks.org>
2021-10-07 21:34:21 -0400

    updating fluentd

---
+++ collection/fluentd.md
@@ -0,0 +1,8 @@
+# Prerequisite
+```
+ansible-galaxy install geerlingguy.fluentd
+```
+Make sure /etc/logrotate.d/nginx.conf has 644 for file permission
+
+# Last test date
+need to test
--- collection/fluentd.sh
+++ collection/fluentd.sh
@@ -19,9 +19,9 @@ export ANSIBLE_ROLES_PATH=./roles:../roles:~/.ansible/roles
 
 
 echo
-read -p "Enter node ip address         : " ip_address
+read -p "Enter node ip address  [192.168.0.104] : " ip_address
 echo
-read -p "Enter ansible_user [debian]   : " ansible_user
+read -p "Enter ansible_user     [debian]        : " ansible_user
 
 ip_address=${ip_address:-192.168.0.104}
 ansible_user=${ansible_user:-debian}
