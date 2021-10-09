5a68a36fc167e02570e5d67dc517020e58ad9093
little-unit-9283 <little-unit-9283@curiosityworks.org>
2021-10-09 01:00:32 -0400

    updating public-fileserver

--- collection/public-fileserver.sh
+++ collection/public-fileserver.sh
@@ -17,17 +17,18 @@ export ANSIBLE_ROLES_PATH=./roles:../roles
 #     exit 0
 # fi
 
-cp ~/.ssh/authorized_keys roles/nginx/files/.ssh/
+cp ~/.ssh/authorized_keys roles/public-fileserver/files/.ssh/
 
 echo
-read -p "Enter node ip address : " ip_address
+read -p "Enter node ip address  [192.168.0.118] : " ip_address
+
+echo
+read -p "Enter ansible_user     [debian]        : " ansible_user
+
 ip_address=${ip_address:-192.168.0.118}
-
-echo
-read -p "Enter ansible_user [debian]: " ansible_user
 ansible_user=${ansible_user:-debian}
 
-cat hosts.tpl > hosts
+cat ./hosts.tpl > hosts
 echo "${ip_address}" >> hosts 
 
 ansible-playbook -i hosts -e "ansible_user=${ansible_user}" public-fileserver.yml   
