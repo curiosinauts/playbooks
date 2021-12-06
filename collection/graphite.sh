218dd64cda45c69eb5c29a1ce9567eb18c1fe010
little-unit-9283 <little-unit-9283@curiosityworks.org>
2021-12-06 13:38:13 -0500

    updating graphite

---
+++ collection/graphite.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -e
+
+# set -x
+
+usage() {
+  echo -e "Usage   : deloy.sh"  
+  echo
+  echo -e 'Example : deloy.sh'
+  echo
+}
+
+export ANSIBLE_STDOUT_CALLBACK=debug
+export ANSIBLE_ROLES_PATH=./roles:../roles
+
+# if [ "${1}" = "" ]; then
+#     usage
+#     exit 0
+# fi
+
+echo
+read -p "Enter node ip address  [192.168.0.115] : " ip_address
+ip_address=${ip_address:-192.168.0.115}
+
+echo
+read -p "Enter ansible_user     [debian]        : " ansible_user
+ansible_user=${ansible_user:-debian}
+
+cat ./hosts.tpl > hosts
+echo "${ip_address}" >> hosts 
+
+ansible-playbook -i hosts -e "setup_option=${1} ansible_user=${ansible_user}" graphite.yml    
+
+rm -f hosts || true
