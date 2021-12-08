79046d4d9d78913772b4e2ae9eaa3925591229f3
little-unit-9283 <little-unit-9283@curiosityworks.org>
2021-12-08 12:48:42 -0500

    fixing graphite and grafana

--- collection/roles/graphite/files/graphite-run.sh
+++ collection/roles/graphite/files/graphite-run.sh
@@ -3,9 +3,9 @@
 set -x
 
 rm -f /opt/graphite/storage/carbon-cache-a.pid
-exec /usr/bin/python /opt/graphite/bin/carbon-cache.py start --debug 2>&1 >> /var/log/carbon.log &
+exec /usr/bin/python3 /opt/graphite/bin/carbon-cache.py start --debug 2>&1 >> /var/log/carbon.log &
 
 rm -f /opt/graphite/storage/carbon-aggregator-a.pid
-exec /usr/bin/python /opt/graphite/bin/carbon-aggregator.py start --debug 2>&1 >> /var/log/carbon-aggregator.log &
+exec /usr/bin/python3 /opt/graphite/bin/carbon-aggregator.py start --debug 2>&1 >> /var/log/carbon-aggregator.log &
 
 PYTHONPATH=/opt/graphite/webapp gunicorn wsgi --workers=4 --bind=0.0.0.0:8888 --log-file=/var/log/gunicorn.log --preload --pythonpath=/opt/graphite/webapp/graphite &
