dbc6dada33fbe78352b707271625781bccaac12e
7onetella <7onetella@gmail.com>
2022-03-06 22:35:07 -0500

    updating docker registry

--- collection/docker-registry.sh
+++ collection/docker-registry.sh
@@ -2,45 +2,29 @@
 
 # set -x
 
-usage() {
-  echo -e "Usage   : deloy.sh [<setup_option>]"  
-  echo
-  echo -e 'Example : deloy.sh consul"'
-  echo
-}
-
 export ANSIBLE_STDOUT_CALLBACK=debug
 export ANSIBLE_ROLES_PATH=./roles:../roles
 
-# if [ "${1}" = "" ]; then
-#     usage
-#     exit 0
-# fi
+echo
+read -p "Enter node ip address       [192.168.0.121]                 : " ip_address
 
 echo
-read -p "Enter node ip address       [192.168.0.107]               : " ip_address
+read -p "Enter host_url [https://docker-registry.curiosityworks.org] : " host_url
 
 echo
-read -p "Enter host_url (e.g. https://docker-registry.example.com) : " host_url
+read -p "Enter ansible_user          [debian]                        : " ansible_user
 
 echo
-read -p "Enter ansible_user          [debian]                      : " ansible_user
+read -p "Enter version               [2.8.0]                         : " version
 
-ip_address=${ip_address:-192.168.0.107}
-host_url=${host_url:-"https://docker-registry.int.curiosityworks.org"}
+ip_address=${ip_address:-192.168.0.121}
+host_url=${host_url:-https://docker-registry.curiosityworks.org}
 ansible_user=${ansible_user:-debian}
+version=${version:-2.8.0}
 
 cat ./hosts.tpl > hosts
 echo "${ip_address}" >> hosts 
 
-if [ "${1}" == "consul" ]; then
-  echo
-  read -p "Enter consul node1 hostname : " consul_node1
-  read -p "Enter consul node2 hostname : " consul_node2
-  read -p "Enter consul node3 hostname : " consul_node3
-  echo
-fi
-
-ansible-playbook -i hosts -e "setup_option=${1} host_url=${host_url} ansible_user=${ansible_user} consul_node1=${consul_node1} consul_node2=${consul_node2} consul_node3=${consul_node3}" docker-registry.yml    
+ansible-playbook -i hosts -e "host_url=${host_url} ansible_user=${ansible_user} version=${version}" docker-registry.yml    
 
 rm -f hosts || true
--- collection/roles/docker-registry/files/registry.sh
+++ collection/roles/docker-registry/files/registry.sh
@@ -1,19 +1,4 @@
 #!/bin/bash -e
 
-if test -f /etc/consul.d/registry.tpl; then
-    # create registry.json for consul agent
-	cd /etc/consul.d/
-	UUID=$(uuid)
-	bind_addr=$(ip address | grep eth1 -A 1 | grep inet | awk '{ print $2 }' | cut -d '/' -f1)
-	cat registry.tpl                          | \
-	sed 's/__uuid__/'"${UUID}"'/g'            | \
-	sed 's/__ip__/'"${bind_addr}"'/g'         | \
-	sed 's/__port__/'"5000"'/g'                 \
-	> registry.json    
-	chown consul:consul registry.json
-    # reload consul
-    /usr/local/bin/consul reload
-fi
-
 # start docker registry
 /usr/local/bin/registry serve /etc/docker/registry/config.yml
