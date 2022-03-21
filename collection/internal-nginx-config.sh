b8ce1e9649caec819987c06fa13639af4120f566
7onetella <7onetella@gmail.com>
2022-03-21 01:31:37 -0400

    updating internal nginx

--- collection/internal-nginx-config.sh
+++ collection/internal-nginx-config.sh
@@ -1,21 +1,3 @@
 #!/bin/bash -e
 
-# set -x
-
-export ANSIBLE_STDOUT_CALLBACK=debug
-export ANSIBLE_ROLES_PATH=./roles:../roles
-
-echo
-read -p "Enter node ip address  [192.168.0.120]  : " ip_address
-ip_address=${ip_address:-192.168.0.120}
-
-echo
-read -p "Enter ansible_user     [debian]         : " ansible_user
-ansible_user=${ansible_user:-debian}
-
-cat ./hosts.tpl > hosts
-echo "${ip_address}" >> hosts 
-
-ansible-playbook -i hosts -e "ansible_user=${ansible_user}" internal-nginx-config.yml    
-
-rm -f hosts || true
+. ./common.sh 192.168.0.120 internal-nginx-config
