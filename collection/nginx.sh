bbd9c1baf5e85674e571c8d4ec3a610fbc06d0e5
7onetella <7onetella@gmail.com>
2022-03-23 08:52:36 -0400

    refactoring nginx

--- collection/nginx.sh
+++ collection/nginx.sh
@@ -1,33 +1,3 @@
 #!/bin/bash -e
 
-# set -x
-
-usage() {
-  echo -e "Usage   : deloy.sh"  
-  echo
-  echo -e 'Example : deloy.sh"'
-  echo
-}
-
-export ANSIBLE_STDOUT_CALLBACK=debug
-export ANSIBLE_ROLES_PATH=./roles:../roles
-
-# if [ "${1}" = "" ]; then
-#     usage
-#     exit 0
-# fi
-
-echo
-read -p "Enter node ip address  [192.168.0.100] : " ip_address
-ip_address=${ip_address:-192.168.0.100}
-
-echo
-read -p "Enter ansible_user     [debian]        : " ansible_user
-ansible_user=${ansible_user:-debian}
-
-cat ./hosts.tpl > hosts
-echo "${ip_address}" >> hosts 
-
-ansible-playbook -i hosts -e "ansible_user=${ansible_user}" nginx.yml    
-
-rm -f hosts || true
+. ./common.sh 192.168.0.100 nginx
