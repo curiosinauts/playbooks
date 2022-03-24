4f7ab392c366d6edbdbfb27bc993fe3f56ddc652
7onetella <7onetella@gmail.com>
2022-03-23 23:03:24 -0400

    updating certbot

--- collection/cerbot.sh
+++ collection/cerbot.sh
@@ -6,19 +6,19 @@ export ANSIBLE_STDOUT_CALLBACK=debug
 export ANSIBLE_ROLES_PATH=./roles:../roles
 
 echo
-read -p "Enter node ip address  [192.168.0.100]            : " ip_address
+read -p "Enter node ip address  [192.168.0.100]     : " ip_address
 ip_address=${ip_address:-192.168.0.100}
 
 echo
-read -p "Enter ansible_user     [debian]                  : " ansible_user
+read -p "Enter ansible_user     [debian]            : " ansible_user
 ansible_user=${ansible_user:-debian}
 
 echo
-read -p "Enter email            [cloudflare email]        : " email
+read -p "Enter email            [cloudflare email]  : " email
 email=${email:-admin@curiosityworks.org}
 
 echo
-read -p "Enter cloudflare api_key                          : " api_key
+read -p "Enter cloudflare api_key                   : " api_key
 
 cat ./hosts.tpl > hosts
 echo "${ip_address}" >> hosts 
