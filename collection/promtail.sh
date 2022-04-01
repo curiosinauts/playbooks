8a190c60ccc6e9b0f66536f83674fd9578e5ce1c
7onetella <7onetella@gmail.com>
2022-04-01 17:18:18 -0400

    improving logging

--- collection/promtail.sh
+++ collection/promtail.sh
@@ -6,15 +6,15 @@ export ANSIBLE_STDOUT_CALLBACK=debug
 export ANSIBLE_ROLES_PATH=./roles:../roles:~/.ansible/roles
 
 echo
-read -p "Enter vm instance ip address  [192.168.69.100]       : " ip_address
+read -p "Enter vm instance ip address  [192.168.69.100]                 : " ip_address
 echo
-read -p "Enter ansible_user            [debian]              : " ansible_user
+read -p "Enter ansible_user            [debian]                         : " ansible_user
 echo
-read -p "Enter loki host               [192.168.69.130]       : " loki_host
+read -p "Enter loki host               [192.168.69.130]                 : " loki_host
 echo
-read -p "Enter job label               [nginxlogs]           : " job_label
+read -p "Enter job label               [nginxlogs]                      : " job_label
 echo
-read -p "Enter log files path          [e.g. /var/log/nginx] : " log_path
+read -p "Enter log files path          [e.g. /var/log/nginx/access.log] : " log_path_expression
 
 ip_address=${ip_address:-192.168.69.100}
 
@@ -24,12 +24,12 @@ loki_host=${loki_host:-192.168.69.130}
 
 job_label=${job_label:-nginxlogs}
 
-log_path=${log_path:-/var/log/nginx}
+log_path_expression=${log_path_expression:-/var/log/nginx/access.log}
 
 cat ./hosts.tpl > hosts
 echo ${ip_address} >> hosts 
 
-ansible-playbook -i hosts -e "loki_host=${loki_host} job_label=${job_label} log_path=${log_path}" promtail.yml    
+ansible-playbook -i hosts -e "loki_host=${loki_host} job_label=${job_label} log_path_expression=${log_path_expression}" promtail.yml    
 
 rm -f hosts
 
