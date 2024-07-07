0e1f295c111111501283cd8a6b4366433c090110
Scott Seo <scott.seo@paramount.com>
2024-07-07 09:27:26 -0400

    updating postgresql

--- collection/postgres.sh
+++ collection/postgres.sh
@@ -6,31 +6,39 @@ export ANSIBLE_STDOUT_CALLBACK=debug
 export ANSIBLE_ROLES_PATH=./roles:../roles:~/.ansible/roles
 
 echo
-read -p "Enter vm instance ip address  [postgresdb]        : " ip_address
+read -p "Enter vm instance ip address  [192.168.10.101]     : " ip_address
+
 echo
-read -p "Enter db name                 [curiosityworks]    : " db_name
+read -p "Enter db name                 [testdb]            : " db_name
+
 echo
-read -p "Enter db user name            [platformctl]       : " user_name
+read -p "Enter db user name            [admin]             : " user_name
+
 echo
 read -p "Enter db user password                            : " user_password
+
 echo
 read -p "Enter ansible_user            [debian]            : " ansible_user
+
 echo
-read -p "Enter postgresql version      [11]                : " postgresql_version
+read -p "Enter postgresql version      [14]                : " postgresql_version
 
-ip_address=${ip_address:-postgresdb}
 
-db_name=${db_name:-curiosityworks}
+ip_address=${ip_address:-192.168.10.101}
 
-user_name=${user_name:-platformctl}
+db_name=${db_name:-testdb}
+
+user_name=${user_name:-admin}
 
 ansible_user=${ansible_user:-debian}
 
-postgresql_version=${postgresql_version:-11} 
+postgresql_version=${postgresql_version:-14} 
 
 cat ./hosts.tpl > hosts
 echo ${ip_address} >> hosts 
 
-ansible-playbook -i hosts -e "db_name=${db_name} user_name=${user_name} user_password=${user_password} postgresql_version=${postgresql_version}" postgres.yml    
+ansible-playbook -i hosts -K -e "db_name=${db_name} user_name=${user_name} user_password=${user_password} postgresql_version=${postgresql_version}" postgres.yml    
+
+# --become-password-file sudo_password.txt
 
 rm -f hosts
