679be733da946ee5ce710cc21d1f935d433e47f8
Scott Seo <scott.seo@paramount.com>
2024-07-13 23:17:21 -0400

    fixing postgresql socket deactivation

--- collection/postgresql-proxy.sh
+++ collection/postgresql-proxy.sh
@@ -15,5 +15,17 @@ ip_address=${ip_address:-192.168.10.101}
 cat ./hosts.tpl > hosts
 echo ${ip_address} >> hosts
 
+set -x
+
+sudo systemctl stop -f postgresql-proxy.socket || true
+sudo systemctl stop -f postgresql-proxy.service || true
+sudo systemctl stop -f postgresql.service || true
+
 ansible-playbook -i hosts ${extra_option} -e "username=${username}" postgresql-proxy.yml
 # ansible-playbook -i hosts -e "username=foo ansible_sudo_pass=bar" postgresql-proxy.yml
+
+sudo systemctl daemon-reload
+
+sudo systemctl start postgresql-proxy.socket
+sudo systemctl start postgresql.service
+sudo systemctl start postgresql-proxy.service
